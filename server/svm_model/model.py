1d94abd45a50ded5a3daf50ad9789786fe598b6e
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-10-23 09:11:30 +0530

    Update model.py

--- server/svm_model/model.py
+++ server/svm_model/model.py
@@ -3,28 +3,38 @@ import random
 import constants
 
 
-def predict(l):
+def predict(season):
 
-    model = joblib.load("svm_model/svm_model.joblib")
+    model = joblib.load("server\svm_model\svm_model.joblib")
 
+    
+    
     N = constants.NITROGEN
     P = constants.PHOSPHORUS
     K = constants.POTASSIUM
     ph = constants.PH
+    
+    tempr = constants.AVERAGE_WEATHER[season].keys()
+    
+    tempr = random.choice(list(tempr))
+    
+    tempr = constants.AVERAGE_WEATHER[season][tempr]
 
     df = [
         [
             random.choice(N),
             random.choice(P),
             random.choice(K),
-            l[0],
-            l[1],
+            tempr, # Temp
+            constants.HUMIDITY, # humid
             random.choice(ph),
-            l[2],
+            constants.RAINFALL# rainfall
         ]
     ]
     prediction = model.predict(df)
     return prediction[0]
 
+
+print(predict("winter"))
 
 # print(predict([20.879744, 82.002744, 202.935536]))

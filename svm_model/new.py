6a9c3d04aed76d78350d29303d52937d7944d69f
Jai Khurana <64320051+jaikhuranna@users.noreply.github.com>
2022-10-23 11:03:45 +0530

    removed print predict from new.py

--- svm_model/new.py
+++ svm_model/new.py
@@ -16,8 +16,3 @@ def predict(l):
     df = [[random.choice(N) ,random.choice(P),random.choice(K),l[0] ,l[1] ,random.choice(ph), l[2]]]
     prediction = model.predict(df)
     return prediction[0]
-
-
-print(predict([20.879744,82.002744,202.935536]))
-
-

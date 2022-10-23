b694fa3ad6bc25ea8157d188c0e13fce9a5cb8b1
Jatin Kumar <main.jatink@gmail.com>
2022-10-23 08:47:07 +0530

    made some changes

--- svm_model/model.py
+++ svm_model/model.py
@@ -4,20 +4,27 @@ import constants
 
 
 def predict(l):
-  
-    model = joblib.load('svm_model\svm_model.joblib')
+
+    model = joblib.load("svm_model\svm_model.joblib")
 
     N = constants.NITROGEN
     P = constants.PHOSPHORUS
     K = constants.POTASSIUM
     ph = constants.PH
-    
-    
-    df = [[random.choice(N) ,random.choice(P),random.choice(K),l[0] ,l[1] ,random.choice(ph), l[2]]]
+
+    df = [
+        [
+            random.choice(N),
+            random.choice(P),
+            random.choice(K),
+            l[0],
+            l[1],
+            random.choice(ph),
+            l[2],
+        ]
+    ]
     prediction = model.predict(df)
     return prediction[0]
 
 
 # print(predict([20.879744,82.002744,202.935536]))
-
-
